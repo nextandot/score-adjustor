e297fce455853d031ae5251c5998beaaa567097e
narugit <narusens@gmail.com>
2020-10-11 16:49:31 +0900

    support material-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import React from 'react'
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react'
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
-const Home: FC = () => {
-  return (
-    <div>
-      hoge
-    </div>
-  )
-}
-export default Home;
+import Button from '@material-ui/core/Button';
+
+const Index = () => (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+
+export default Index;
