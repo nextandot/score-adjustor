0865791b76323a37b03ef20c0e7c1741c6b78b94
narugit <narusens@gmail.com>
2020-10-17 14:05:46 +0900

    Add some components material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,77 @@
-import React, {FC} from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
 
-import Button from '@material-ui/core/Button';
+import { useTheme, makeStyles, Theme } from "@material-ui/core/styles";
+import {
+  Toolbar,
+  Typography,
+  AppBar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 
-const Index = () => (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+const useStyle = makeStyles({
+  root: (props: Theme) => ({
+    paddingTop: props.spacing(10),
+    paddingLeft: props.spacing(5),
+    paddingRight: props.spacing(5),
+  })
+});
+
+export default function Home() {
+  const [ dialogOpen, setDialogOpen ] = useState(true);
+  const classes = useStyle(useTheme());
+  return (
+    <div className={classes.root}>
+      <Head>
+        <title>Score Adjustor</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Head>
+      <Dialog open={dialogOpen} onClose={() => {setDialogOpen(false)}}>
+        <DialogTitle>Dialog Sample</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            hoge
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            color="primary"
+            onClick={() => {setDialogOpen(false)}}
+          >OK</Button>
+        </DialogActions>
+      </Dialog>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Header
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="subtitle1" gutterBottom={true}>
+        example project
+      </Typography>
+      <Typography gutterBottom={true}>
+        <Link href="/about">
+          <a>Go to the about page</a>
+        </Link>
+      </Typography>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => { setDialogOpen(true)}}
+      >Shot Dialog</Button>
+      <style jsx={true}>{`
+        .root {
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
-
-export default Index;
+}
